a135eef9f272ac9295cf20ccd99a05ecfba5997a
Bastien <bmoulia@gmail.com>
2026-09-19 16:36:52 +0200

    Update Firebase configuration in environment settings and add analytics to Angular CLI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,11 @@ export interface AppEnvironment {
 export const environment: AppEnvironment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: "AIzaSyDN_rBWJjVupuO4PhttQdy80he_rMFAMbc",
+    authDomain: "bask-learning-app.firebaseapp.com",
+    projectId: "bask-learning-app",
+    storageBucket: "bask-learning-app.firebasestorage.app",
+    messagingSenderId: "218857568810",
+    appId: "1:218857568810:web:d303f146da0a3270f8a153"
   },
 };
