0c1865e51ce38f68b32a76af8c690bf6ad4db914
Bastien <bmoulia@gmail.com>
2026-09-21 22:25:27 +0200

    feat: Add initial iOS project structure with LaunchScreen and Main storyboards
    - Created LaunchScreen.storyboard for app launch interface.
    - Added Main.storyboard for the main app interface.
    - Introduced Info.plist with necessary configurations for the app.
    - Implemented SceneDelegate.swift to manage app lifecycle and scene connections.
    - Added .gitignore for SPM dependencies and build artifacts.
    - Created Package.swift for managing SPM dependencies.
    - Added README.md for CapApp-SPM package documentation.
    - Included CapApp-SPM.swift to define the Capacitor app flag.
    - Created debug.xcconfig for debug configuration settings.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bastienmoulia.bask.learning.app',
+  appName: 'bask-learning-app',
+  webDir: 'undefined'
+};
+
+export default config;
